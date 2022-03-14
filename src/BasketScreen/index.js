4827dd688f2d6dfeebae7afb038e529c16780a62
Mickaël SAMYN <mickael.samyn@cdiscount.com>
2022-03-14 17:12:00 +0100

    feat: 🎸 Add app bar to All screen

--- src/BasketScreen/index.js
+++ src/BasketScreen/index.js
@@ -1,11 +1,5 @@
-import { Link } from "react-router-dom";
 import Screen from "../Screen";
 
 export default function BasketScreen() {
-  return (
-    <Screen
-      title="Panier"
-      navigation={<Link to="/">Revenir à la page d'accueil</Link>}
-    />
-  );
+  return <Screen>Panier</Screen>;
 }
