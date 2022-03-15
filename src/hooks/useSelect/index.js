9aa44bbe1147d9f9a2466ce86dcd48bd22aaf9fc
Mickaël SAMYN <mickael.samyn@cdiscount.com>
2022-03-15 17:43:23 +0100

    fix: 🐛 fix initial state

--- src/hooks/useSelect/index.js
+++ src/hooks/useSelect/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function useSelect(initialValue = 0) {
+export default function useSelect(initialValue = "") {
   const [value, setValue] = React.useState(initialValue);
   const handleChange = (event) => {
     setValue(event.target.value);
