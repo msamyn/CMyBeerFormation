c42583b1573c5e7d7d896a21710156d00a9f3b43
Mickaël SAMYN <mickael.samyn@cdiscount.com>
2022-03-21 15:02:16 +0100

    test: 💍 remove unused data

--- src/App/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from ".";
-
-test("renders app title", () => {
-  render(<App />);
-  const title = screen.getByRole("heading", { level: 1, name: "🍻 CMyBeer" });
-  expect(title).toBeInTheDocument();
-});
